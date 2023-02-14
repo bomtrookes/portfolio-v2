9da37a6a8b740acb96dddd853b3108d32a61e4fc
Tom Brookes <tom.brookes15@gmail.com>
2023-02-14 21:40:49 +0000

    model and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'blogs/index'
+  get 'blogs/show'
+  get 'blogs/new'
+  get 'blogs/edit'
+  get 'projects/index'
+  get 'projects/show'
+  get 'projects/new'
+  get 'projects/edit'
   devise_for :users
 
   root "home#index"
