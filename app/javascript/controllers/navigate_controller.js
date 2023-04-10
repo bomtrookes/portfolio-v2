c8ec0fe95592c6b96d58025795cb15e0ded80731
Tom Brookes <tom.brookes15@gmail.com>
2023-04-10 17:30:27 +0100

    update to projects index

--- app/javascript/controllers/navigate_controller.js
+++ app/javascript/controllers/navigate_controller.js
@@ -6,9 +6,17 @@ export default class extends Controller {
 
   connect() {
     console.log("Navigate controller connected")
+
+    this.containerTarget.addEventListener("scroll", () => {
+      if (this.containerTarget.scrollTop < (this.containerTarget.scrollHeight / 2)) {
+        this.buttonTarget.style.transform = "rotate(0deg)"
+      } else if (this.containerTarget.scrollTop > (this.containerTarget.scrollHeight / 2)) {
+        this.buttonTarget.style.transform = "rotate(180deg)"
+      }
+    })
   }
 
-  scroll() {
+  scroll(e) {
     if(this.containerTarget.scrollTop == 0) {
       this.containerTarget.scrollTop = (this.containerTarget.scrollHeight / 2)
       this.buttonTarget.style.transform = "rotate(180deg)"
