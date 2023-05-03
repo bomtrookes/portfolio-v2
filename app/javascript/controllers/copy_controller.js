222078652180b6a6c75ccb874bdd3ffb40837dff
Tom Brookes <tom.brookes15@gmail.com>
2023-05-03 16:30:08 +0100

    share link button implemented

--- app/javascript/controllers/copy_controller.js
+++ app/javascript/controllers/copy_controller.js
@@ -2,7 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="copy"
 export default class extends Controller {
+  static targets = [ "button" ]
   connect() {
     console.log("copy text")
+  }
+  copyURL(e) {
+    e.preventDefault();
+    const url = window.location.href
+    navigator.clipboard.writeText(url).then(() => {
+      const flash = document.createElement("div");
+      flash.classList.add("prompt-notice");
+      flash.textContent = "Link copied to clipboard!";
+      document.body.appendChild(flash);
+      setTimeout(() => flash.remove(), 3000);
+    });
   }
 }
